98c2e4827382ba5b1f132e8a2bd095c0be08c569
Brian Shaughnessy <brian@lcdservices.biz>
2022-06-10 11:15:29 -0400

    general extension update

--- customfieldnames.php
+++ customfieldnames.php
@@ -12,17 +12,6 @@ function customfieldnames_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param $files array(string)
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function customfieldnames_civicrm_xmlMenu(&$files) {
-  _customfieldnames_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -72,54 +61,6 @@ function customfieldnames_civicrm_disable() {
  */
 function customfieldnames_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _customfieldnames_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function customfieldnames_civicrm_managed(&$entities) {
-  _customfieldnames_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function customfieldnames_civicrm_caseTypes(&$caseTypes) {
-  _customfieldnames_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function customfieldnames_civicrm_angularModules(&$angularModules) {
-_customfieldnames_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function customfieldnames_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _customfieldnames_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -214,4 +155,22 @@ function customfieldnames_civicrm_validateForm($formName, &$fields, &$files, &$f
     }
   }
   return;
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function customfieldnames_civicrm_postInstall() {
+  _customfieldnames_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function customfieldnames_civicrm_entityTypes(&$entityTypes) {
+  _customfieldnames_civix_civicrm_entityTypes($entityTypes);
 }
